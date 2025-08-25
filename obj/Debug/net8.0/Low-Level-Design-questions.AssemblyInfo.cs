501f16882c6c929499c880aaec4cec3e16f28241
Jil-Patel05 <jilpatel2105@gmail.com>
2025-08-25 22:21:29 +0530

    Added parking plot deign.

--- obj/Debug/net8.0/Low-Level-Design-questions.AssemblyInfo.cs
+++ obj/Debug/net8.0/Low-Level-Design-questions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Low-Level-Design-questions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+049071e9c6ffa8e793290e6fd209036a5c967139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Low-Level-Design-questions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Low-Level-Design-questions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
